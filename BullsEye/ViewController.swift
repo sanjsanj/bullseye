2d982d9fb6cc869bd03c5618903cb831a74c6084
Sanjay Purswani <sanjsanj@hotmail.com>
2016-10-22 09:39:55 +0100

    Slider value gets alerted by button

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 50
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,16 +24,22 @@ class ViewController: UIViewController {
 
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
+        
         let alert = UIAlertController(title: "Hello, World",
-                                      message: "This is my first app!",
+                                      message: message,
                                       preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Awesome",
+        let action = UIAlertAction(title: "OK",
                                    style: .default,
                                    handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
 }

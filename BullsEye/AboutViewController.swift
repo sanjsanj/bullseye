bd8ed563150c330ace2675dd48464919f17594ad
Sanjay Purswani <sanjsanj@hotmail.com>
2016-10-22 11:25:11 +0100

    Set up about screen

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Sanjay Purswani on 22/10/2016.
+//  Copyright © 2016 Sanjay Purswani. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
